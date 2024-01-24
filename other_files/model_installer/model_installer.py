604abfc00dbe76a5ba3d52d0d011b707934b86c2
LunaMidori5 <jaredteam@gmail.com>
2024-01-23 23:45:41 -0800

    alright this time for sure

--- other_files/model_installer/model_installer.py
+++ other_files/model_installer/model_installer.py
@@ -1432,6 +1432,7 @@ if answerstartup == 4:
 
             docker_commands = [
                 ["rm", "-f", "yaml_edit.py"],
+                ["pip", "install", "pyyaml"],
                 ["wget", "-O", "yaml_edit.py", "https://tea-cup.midori-ai.xyz/download/yaml_edit.py"],
                 ["python3", "yaml_edit.py", "-i", answeryamleditor_two, "-d", f"\"{answeryamleditor_three.lower()}\"", yaml_path_temp],
             ]
