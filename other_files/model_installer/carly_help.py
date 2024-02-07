74bf1dc47edf289f79b1ebbeca60260c9b93c04b
LunaMidori5 <jaredteam@gmail.com>
2024-02-06 21:25:46 -0800

    autogen tests

--- other_files/model_installer/carly_help.py
+++ other_files/model_installer/carly_help.py
@@ -55,6 +55,8 @@ def request_llm(client_openai, request_in, system_message):
         {"role": "user", "content": request_in}
     ])
 
+    s.log(client_openai.extract_text_or_completion_object(completion))
+
     end_message = client_openai.extract_text_or_completion_object(completion)
 
     return end_message
