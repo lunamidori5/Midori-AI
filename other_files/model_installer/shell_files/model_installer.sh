9af1f3e54a1a48a0a86e28818fc8b1504185af3b
idacat41 <55522479+idacat41@users.noreply.github.com>
2024-04-08 13:06:21 -0400

    Update model_installer.sh (#15)
    Updated the linux model installer to be more automated.

--- other_files/model_installer/shell_files/model_installer.sh
+++ other_files/model_installer/shell_files/model_installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # download, extract and execute model_installer
+sub_man_dir=$(pwd)
 curl -sSL https://tea-cup.midori-ai.xyz/download/model_installer_linux.tar.gz -o subsystem_manager.tar.gz
-tar -xzf subsystem_manager.tar.gz
-chmod +x subsystem_manager
-./subsystem_manager
+tar -xzf $sub_man_dir/subsystem_manager.tar.gz
+chmod 755 $sub_man_dir/subsystem_manager
+$sub_man_dir/subsystem_manager
