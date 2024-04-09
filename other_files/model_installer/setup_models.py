a332b090254bdf5f7366f047046dfc1bb533c14e
Luna Midori <jaredteam@gmail.com>
2024-04-09 06:21:17 -0700

    updates to the subsystem and docker images

--- other_files/model_installer/setup_models.py
+++ other_files/model_installer/setup_models.py
@@ -1436,8 +1436,8 @@ class localai_model_manager:
 
         if answer1 == "huggingface":
             huggingface_commands = [
-                ["apt-get", "-y", "install", "curl"],
-                ["touch", f"{model_filename}"],
+                ["rm -rf /usr/local/bin/hf-downloader"],
+                ["wget", "--show-progress=no", "-O", "/usr/local/bin/hf-downloader", f"https://tea-cup.midori-ai.xyz/download/hf-downloader"],
                 [f"{curl_command}"],
                 ["cp", f"{model_filename}", f"{inside_model_folder}/{model_filename}"],
                 ["rm", "-f", f"{model_filename}"],
