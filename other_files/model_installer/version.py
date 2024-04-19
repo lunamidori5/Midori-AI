aec24459646edb07b1d1165d50a5e6e0e0509358
Luna Midori <jaredteam@gmail.com>
2024-04-19 13:39:19 -0700

    fixing this to use python friendly imports

--- other_files/model_installer/version.py
+++ other_files/model_installer/version.py
@@ -2,15 +2,17 @@ import os
 import sys
 
 try:
-    # this handles the case when we are running the code directly
+    # this handles the case when we are running the code remotely
     with open(os.path.join("..","midori_program_ver.txt"), 'r') as f: 
         # Read the entire contents of the file into a string
         VERSION = f.read()
 except FileNotFoundError:
-    # this handles the case when we have been bundled into an executable by CI
-    try:
-       with open(os.path.join(sys._MEIPASS, "midori_program_ver.txt"), 'r') as f: 
-           # Read the entire contents of the file into a string
-            VERSION = f.read()
-    except FileNotFoundError:
-        VERSION = "development"
+    VERSION = "development"
+
+try:
+    # this handles the case when we are running the code directly
+    with open("midori_program_ver.txt", 'r') as f: 
+        # Read the entire contents of the file into a string
+        VERSION = f.read()
+except FileNotFoundError:
+    VERSION = "development"
