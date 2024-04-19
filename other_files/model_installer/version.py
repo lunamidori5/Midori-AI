0e12720771ecca9d81437fd21dccf6be83f2188a
Luna Midori <jaredteam@gmail.com>
2024-04-19 13:46:19 -0700

    added a few else statments

--- other_files/model_installer/version.py
+++ other_files/model_installer/version.py
@@ -3,16 +3,18 @@ import sys
 
 try:
     # this handles the case when we are running the code remotely
-    with open(os.path.join("..","midori_program_ver.txt"), 'r') as f: 
-        # Read the entire contents of the file into a string
-        VERSION = f.read()
-except FileNotFoundError:
-    VERSION = "development"
-
-try:
-    # this handles the case when we are running the code directly
-    with open("midori_program_ver.txt", 'r') as f: 
-        # Read the entire contents of the file into a string
-        VERSION = f.read()
+    if os.path.exists(os.path.join("..","midori_program_ver.txt")):
+        with open(os.path.join("..","midori_program_ver.txt"), 'r') as f: 
+            # Read the entire contents of the file into a string
+            VERSION = f.read()
+    
+    elif os.path.exists("midori_program_ver.txt"):
+        with open("midori_program_ver.txt", 'r') as f: 
+            # Read the entire contents of the file into a string
+            VERSION = f.read()
+    
+    else:
+        VERSION = "development"
+        
 except FileNotFoundError:
     VERSION = "development"
