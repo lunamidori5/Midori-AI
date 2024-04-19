bdc8cb0a5864542615131107366b4321ee69b2d2
Chris Jowett <421501+cryptk@users.noreply.github.com>
2024-04-18 22:49:10 -0500

    fix version handling in artifacts

--- other_files/model_installer/version.py
+++ other_files/model_installer/version.py
@@ -1,3 +1,16 @@
-with open("../midori_program_ver.txt", 'r') as f: 
-    # Read the entire contents of the file into a string
-    VERSION = f.read()
+import os
+import sys
+
+try:
+    # this handles the case when we are running the code directly
+    with open(os.path.join("..","midori_program_ver.txt"), 'r') as f: 
+        # Read the entire contents of the file into a string
+        VERSION = f.read()
+except FileNotFoundError:
+    # this handles the case when we have been bundled into an executable by CI
+    with open(os.path.join(sys._MEIPASS, "midori_program_ver.txt"), 'r') as f: 
+        # Read the entire contents of the file into a string
+        VERSION = f.read()
+finally:
+    # all else fails, fall back to "development"
+    VERSION = "development"
