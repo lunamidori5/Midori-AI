e7d2e56abe9c06b477b877cfb4f0fc334bd68524
Chris Jowett <421501+cryptk@users.noreply.github.com>
2024-04-18 23:02:53 -0500

    fix version logic

--- other_files/model_installer/version.py
+++ other_files/model_installer/version.py
@@ -8,9 +8,9 @@ try:
         VERSION = f.read()
 except FileNotFoundError:
     # this handles the case when we have been bundled into an executable by CI
-    with open(os.path.join(sys._MEIPASS, "midori_program_ver.txt"), 'r') as f: 
-        # Read the entire contents of the file into a string
-        VERSION = f.read()
-finally:
-    # all else fails, fall back to "development"
-    VERSION = "development"
+    try:
+       with open(os.path.join(sys._MEIPASS, "midori_program_ver.txt"), 'r') as f: 
+           # Read the entire contents of the file into a string
+            VERSION = f.read()
+    except FileNotFoundError:
+        VERSION = "development"
