540aaca83888a6f96fa5c3307d288fa452bdfbeb
lunamidori <jaredteam@gmail.com>
2024-05-14 02:13:20 +0000

    fixed up a few things ao that everything syncs nicely

--- other_files/Lunasmixedprograms/syncfiles.py
+++ other_files/Lunasmixedprograms/syncfiles.py
@@ -6,6 +6,19 @@ windows_path = "windows"
 winxlinux_temp_path = "winxlinux_temp"
 server1_path = "server1"
 
+def pre_sync_folder(source, destination):
+    """Syncs a source folder to a destination folder using rsync."""
+    rsync_command = [
+        "rsync",
+        "-a",  # archive mode: preserves permissions, timestamps, etc.
+        "-v",  # verbose output
+        "-h",
+        "-z",
+        source + "/",  # trailing slash ensures directory contents are synced
+        destination
+    ]
+    subprocess.run(rsync_command)
+
 def sync_folder(source, destination):
     """Syncs a source folder to a destination folder using rsync."""
     rsync_command = [
@@ -20,11 +33,14 @@ def sync_folder(source, destination):
     ]
     subprocess.run(rsync_command)
 
-while True:
-    # Sync "windows" to "winxlinux_temp"
-    sync_folder(windows_path, winxlinux_temp_path)
+pre_sync_folder(windows_path, winxlinux_temp_path)
+pre_sync_folder(server1_path, winxlinux_temp_path)
 
-    # Sync "server1" to "winxlinux_temp"
+while True:
+    sync_folder(windows_path, winxlinux_temp_path)
     sync_folder(server1_path, winxlinux_temp_path)
+    
+    sync_folder(winxlinux_temp_path, windows_path)
+    sync_folder(winxlinux_temp_path, server1_path)
 
     time.sleep(2)  # Wait for 2 seconds before syncing again
