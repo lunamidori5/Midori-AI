7c47122a53bcd03bbf71163f386a3bbf373da483
Luna Midori <jaredteam@gmail.com>
2025-03-08 00:04:08 -0800

    fix: update video generation parameters and prompts for improved output quality

--- Experimentation/text-to-video/main.py
+++ Experimentation/text-to-video/main.py
@@ -10,7 +10,10 @@ from diffusers.utils import load_image
 from diffusers.utils import export_to_gif
 from diffusers.utils import export_to_video
 
+pipeline_list = []
+
 #pipeline = I2VGenXLPipeline.from_pretrained("ali-vilab/i2vgen-xl", torch_dtype=torch.float16, variant="fp16")
+#pipeline = LTXPipeline.from_pretrained("Lightricks/LTX-Video", torch_dtype=torch.bfloat16)
 pipeline = LTXImageToVideoPipeline.from_pretrained("Lightricks/LTX-Video", torch_dtype=torch.bfloat16)
 
 #pipeline.unet.enable_forward_chunking()
@@ -23,23 +26,21 @@ image = load_image(image_url).convert("RGB")
 old_prompt = "(Black strapless dress, White cloak), 1woman, (dark green hair), bangs, closed mouth, (golden yellow eyes)+, (small Halo), walking towards camera"
 
 prompt = """
-Cinematic shot of a woman walking towards the camera. 
-The scene is a cave filled with ethereal light. 
-She is wearing a black strapless dress and a white cloak. 
-Dark green hair with bangs frames her face. 
-Her eyes are a striking golden yellow, and a delicate halo adorns her head. 
-She moves with a magical aura, a gentle wave of distorted light rippling behind her as she progresses through the cave.
+A woman in walking.
+She is wearing a black strapless dress and a white cloak. Dark green hair with bangs frames her face. 
+Her eyes are a striking golden yellow, and a delicate golden halo adorns her head. 
+She has a sliver staff she is using to walk.
+She walks through the town, from left to right.
 """.replace("\n", " ")
 
 negative_prompt = "Distorted, discontinuous, Ugly, blurry, low resolution, motionless, static, disfigured, disconnected limbs, Ugly faces, incomplete arms"
-generator = torch.manual_seed(random.randint(1, 99999))
+generator = torch.manual_seed(8585)
 
-output = pipeline(
-    prompt=prompt, negative_prompt=negative_prompt, 
-    image=image, num_inference_steps=45, guidance_scale=9.0, num_frames=75, 
-    height=1280, width=704, generator=generator)
+output = pipeline(prompt=prompt, negative_prompt=negative_prompt, image=image, num_inference_steps=150, num_frames=120, frame_rate=24, height=704, width=480, generator=generator)
+
+#output = pipeline(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=150, num_frames=120, frame_rate=24, generator=generator)
 
 frames = output.frames[0]
 
-export_to_gif(frames, "generated.gif", fps=2)
-export_to_video(frames, "generated.mp4", fps=2)
+export_to_gif(frames, "generated.gif", fps=24)
+export_to_video(frames, "generated.mp4", fps=24)
